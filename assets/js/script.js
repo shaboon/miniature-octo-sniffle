3a2b602c7b8e3be3d15301d561dae9fc16294280
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-03-15 10:55:45 -0400

    Added dayjs Clock on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
-var today = dayjs();
-$('.clock').text(today.format('MMM D, YYYY'));
+  var clock = setInterval(function () {
+    var time = dayjs().format('MMM D, YYYY, hh:mm:ss a');
+    $('.clock').text(time);
+
+    console.log('Tick ${time} tock')
+}, 1000);
